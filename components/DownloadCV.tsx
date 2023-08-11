094c5e14d679c91295768934263c4f922a120a8e
geromedina <gerocode768@gmail.com>
2023-08-11 16:23:14 -0300

    update cv

--- components/DownloadCV.tsx
+++ components/DownloadCV.tsx
@@ -5,10 +5,17 @@ import { useTranslation } from "react-i18next"
 
 const DownloadCV = () => {
     const [t, i18n] = useTranslation("global");
+
+    const getDownloadLink = () => {
+        const language = i18n.language;
+        const fileName = language === 'es' ? 'CV - Spanish.pdf' : 'CV - English.pdf';
+        return `./${fileName}`;
+    };
+
     return (
         <span>
             <a
-                href="./CV - Geronimo Medina.pdf"
+                href={getDownloadLink()}
                 download
                 className=" hover:-translate-y-1 transition-transform inline-block bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 hover:from-indigo-600 hover:via-purple-600 hover:to-pink-600 text-white font-semibold px-4 py-3 rounded shadow cursor-pointer"
             >
